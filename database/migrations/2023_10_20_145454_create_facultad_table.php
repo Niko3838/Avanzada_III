328d1ebf80f3ff1ca90faf802a432055cf82d288
Nixon Benavides <lubierb383@gmail.com>
2023-10-20 10:10:56 -0500

    migraciones

---
+++ database/migrations/2023_10_20_145454_create_facultad_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facultad', function (Blueprint $table) {
+            $table->string('codfacultad',2);
+            $table->string('nomfacultad',100);
+            $table->timestamps();
+            $table->primary('codfacultad');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facultad');
+    }
+};
